97d763da76e8e4953d5b81d34aa03ce8a84fef36
Nandish Rao <nandishhraoo@gmail.com>
2026-07-19 15:09:12 +0530

    Security config for sessions and HTTP requests

---
+++ src/main/java/com/healthcare/healthcare_management/config/SecurityConfig.java
@@ -0,0 +1,37 @@
+package com.healthcare.healthcare_management.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableMethodSecurity
+public class SecurityConfig {
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        HttpSecurity httpSecurity = http
+                .csrf(csrf -> csrf.disable())
+
+                .sessionManagement(session ->
+                        session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/api/public/**").permitAll()
+                        .anyRequest().authenticated()
+                )
+
+                .oauth2ResourceServer(oauth ->
+                        oauth.jwt(jwt ->
+                                jwt.jwtAuthenticationConverter(new KeycloakJwtRoleConverter())
+                        )
+                );;
+
+        return http.build();
+    }
+}
---
+++ src/main/java/com/healthcare/healthcare_management/controller/PublicController.java
@@ -0,0 +1,15 @@
+package com.healthcare.healthcare_management.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/public")
+public class PublicController {
+
+    @GetMapping("/health")
+    public String health() {
+        return "Healthcare API Running";
+    }
+}
