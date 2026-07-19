4595c0685bd7b4fe9a891843f961d39011feef15
Nandish Rao <nandishhraoo@gmail.com>
2026-07-19 15:05:39 +0530

    a verifying routes

---
+++ src/main/java/com/healthcare/healthcare_management/controller/HelloController.java
@@ -0,0 +1,12 @@
+package com.healthcare.healthcare_management.controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Healthcare API Running";
+    }
+}
